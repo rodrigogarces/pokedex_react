438360c7888b4438762abd40cf7c9558c76e8e9e
Rodrigo Garcês <rmgarces1@gmail.com>
2022-06-06 01:00:01 -0300

    Adicionado sprite vetorial

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,7 +2,8 @@ class Pokemon {
     constructor(data) {
         this.id = data.id;
         this.name = data.name;
-        this.sprite = data.sprites.front_default;
+        // this.sprite = data.sprites.front_default;
+        this.sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.id}.svg`
         this.type = data.types[0].type.name;
         this.height = data.height * 10;
         this.weight = data.weight / 10;
