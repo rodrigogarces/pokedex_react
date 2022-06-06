5c57e6f7ec9241f94315ae6013dfa2c5b3ef8553
Rodrigo Garcês <rmgarces1@gmail.com>
2022-06-05 23:56:54 -0300

    Pokedex Funcional

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import './styles/App.css'
 import PokeList from './PokeList'; //lista todos os pokémons
 import DetailView from './DetailView'; //visão detalhada de um pokémon
-import './styles/App.css';
+import './styles/App.css'; //estilo da página
+import Pokemon from '../Pokemon'; //helper que recebe os dados da API
 class App extends Component {
   constructor() { //incialização do estado do componente
     super();
-    this.state = {};
+    this.state = {
+      pokemon: {} //instancia o pokémon a ser mostrado no detalhe
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this); //mantém o contexto quando envia
   }
+
+  handleOnClick(id){ //pega o id do elemento clicado
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+
+        // console.log(pokemon);
+        this.setState({pokemon});
+      })
+      .catch(err => console.log(err));
+  }
+
   render() { //retorna o elemento criado
-    return (
+    return ( //PokeList recebe o id do elemento clicado
       <div className = "App">
-        <PokeList />
-        <DetailView />
+        <PokeList handleOnClick = {this.handleOnClick} />
+        <DetailView pokemon={this.state.pokemon} />
       </div>
     );
   }
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PokeCell from './PokeCell';
 import { pokeClasses } from '../PokeClasses';
 import './styles/PokeList.css';
-const PokeList = () => {
+const PokeList = ({handleOnClick}) => {
     const cells = pokeClasses.map(pokeClass => {
         return (
             <PokeCell 
                 key = {pokeClass.id}
                 pokeClass={pokeClass}
+                handleOnClick = {handleOnClick} //recebe o id clicado
             />
         );
     });
